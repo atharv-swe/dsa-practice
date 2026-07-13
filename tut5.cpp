1eaab1ea3ce4a584331ff19c78680d87d9f172f1
Atharv <atharvbhende25@gmail.com>
2026-07-13 10:43:37 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;    
+int main() { 
+    int num1, num2, sum;
+  cout << "Enter the value of num1: ";
+  cin >> num1;
+  cout << "Enter the value of num2: ";
+  cin >> num2;
+  sum = num1 + num2;
+  cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+  return 0;
+}
